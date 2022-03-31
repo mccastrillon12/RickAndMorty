9cde148dda161a387ba03477414e71849ce3e622
camila <castrillonosorio12@gmail.com>
2022-03-31 16:13:37 -0500

    change text img episodes

--- src/app/models/videoLibrary.ts
+++ src/app/models/videoLibrary.ts
@@ -3,15 +3,15 @@ import { Video } from "./video";
 export class VideoLibrary{
   video: Video [] = [{
     url: "https://www.youtube.com/embed/dYlV1iCL9Kg",
-    image: "../assets/img/video1.PNG",
+    image: 'assets/img/video1.PNG',
   },
   {
     url: "https://www.youtube.com/embed/z9rKwdRpEO0",
-    image: "../assets/img/video2.PNG",
+    image: 'assets/img/video2.PNG',
   },
   {
     url: "https://www.youtube.com/embed/S92xxHScMwE",
-    image: "../assets/img/video3.PNG",
+    image: 'assets/img/video3.PNG',
   }
 
 
